39328b6dddd9ff9d5c99e7e04b2a2ab04535434f
hugoresende27 <Hugoresende27@gmail.com>
2022-02-11 09:28:14 +0000

    manha

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -2,6 +2,22 @@
 
 @section('content')
 
+<style>
+
+    .meuBtn:hover{
+        background-color: darkgreen;
+        color: white;
+        border: none;
+        transform: scale(1.5);
+    }
+
+</style>
+
+
+<div class="text-center mb-4">
+    <a href="/posts" class="btn btn-info meuBtn">Voltar</a>
+</div>    
+
     <div class="card text-white p-3" style="background-color: rgb(1, 26, 31)">
 
             <h1>{{ $post->title }}</h1>   
